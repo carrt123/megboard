35660e19b8169895bd56ae8e83b0a6fe506cf9a8
carrt123 <sam@globadevops.io>
2025-08-06 09:14:30 +0800

    撤除上传idea文件

--- configs.py
+++ configs.py
@@ -1,3 +1,4 @@
 SECRET_KEY = "This is a SECRET"
-DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+# SQLite 数据库连接 URI，数据库文件将存储在项目根目录下的 megboard.db
+DB_URI = 'sqlite:///megboard.db'
 SQLALCHEMY_DATABASE_URI = DB_URI
