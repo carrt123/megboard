f29c884fda6eec91ea3548dbeb0daf53e8a0f0f2
carrt123 <sam@globadevops.io>
2025-08-06 09:26:15 +0800

    chore: 完善app文件

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+import configs
 
 app = Flask(__name__)
-app.config.from_object()
+app.config.from_object(configs)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
