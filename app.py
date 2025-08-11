b7d00396c10449b164fb4a920e5c289738569885
carrt123 <sam@globadevops.io>
2025-08-11 20:38:04 +0800

    chore:完善html信息

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
+from exts import db, moment, bootstrap
 import configs
 
 
-app = Flask(__name__)
-app.config.from_object(configs)
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(configs)
+    # 其他配置...
+    # 动态绑定app
+    db.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
 
-db = SQLAlchemy(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-from models import Message
-import commands  # 新增这行
+    # 导入本地模块（在app初始化后）
+    from models import Message
+
+    return app
+
+
+app = create_app()
+import commands, views, errors
 if __name__ == '__main__':
-    app.run()
+
+    app.run(debug=True)
